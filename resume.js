c2bc1e292d15cac8faecf68881df58a26ae72d35
Sundaravel-3003 <110889723+Sundaravel-3003@users.noreply.github.com>
2022-08-17 19:11:46 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,49 @@
+
+
+let myResume = {
+  "basics" : {
+    "name" : "sundaravel",
+    "age" : 23,
+    "mobile number" : 9047471014,
+    "location" : {
+      "flat number" : 6,
+      "block number" : "B block",
+      "apartment" : "gajanan apartments",
+      "street" : " 5th cross street",
+      "area" : "krishna nagar,pammal",
+      "city" : "chennai",
+      "state" : "tamilnadu",
+      "country" : "india",
+      "pincode" : 600075,
+    },
+    "profile" : {
+      "github" : "https://github.com/Sundaravel-3003"
+    },
+  },
+  "education" : [
+    {
+      "degree" : "BE",
+      "college name" : "velammal engineering college",
+      "course" : "mechanical engineering",
+      "batch" : "2016-2020",
+      "passed out year" : 2020,
+      "gpa" : 7.5,
+
+  }
+],
+"skills" : [
+  {
+    "languages known" : "html,css,js",
+    "level" : "beginner",
+    "undergoing studies" : "full stack developement",
+    "institute name" : "guvi",
+  }
+],
+"interest" : ["drawing","cricket","coding","learing new things"],
+
+"languages known" : ["tamil","english"],
+
+     
+}
+
+console.log(myResume);
